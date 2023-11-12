503793944572e12effb1b39d2c5a95710224b1a5
박성춘 <songchon100@gmail.com>
2023-11-12 15:25:34 +0900

    feat : 에러 메시지 출려 기능 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -3,4 +3,8 @@ package christmas.view;
 public class OutputView {
 
     public OutputView() {}
+
+    public void printErrorMessage(IllegalArgumentException e) {
+        System.out.println(e.getMessage());
+    }
 }
