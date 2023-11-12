d3c89ee9dc2e4a490fff867d99b4b19f472889fb
박성춘 <songchon100@gmail.com>
2023-11-12 16:34:56 +0900

    feat : 각 메뉴타입에 대한 개수체킹 객체 생성

---
+++ src/main/java/christmas/domain/order/OrderStatus.java
@@ -0,0 +1,32 @@
+package christmas.domain.order;
+
+import christmas.constant.MenuType;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+public class OrderStatus {
+    private Map<MenuType, Integer> orderMenuTypeAndNum;
+
+    public OrderStatus() {
+        orderMenuTypeAndNum = new EnumMap<>(MenuType.class);
+        for (MenuType menuType : MenuType.values()) {
+            orderMenuTypeAndNum.put(menuType, 0);
+        }
+    }
+
+    public void updateMenuTypeNum(MenuType menuType) {
+        orderMenuTypeAndNum.put(menuType, orderMenuTypeAndNum.get(menuType) + 1);
+    }
+
+    public boolean isOnlyDrink() {
+        for (MenuType menuType : MenuType.values()) {
+            if (menuType != MenuType.DRINK && orderMenuTypeAndNum.get(menuType) > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+}
