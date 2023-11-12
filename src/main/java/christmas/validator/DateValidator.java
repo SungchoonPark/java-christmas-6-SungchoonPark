fabf6abccc93e2e12971a45ac87b341089e4fa02
박성춘 <songchon100@gmail.com>
2023-11-12 15:27:51 +0900

    feat : 방문 날짜에 대한 유효성 검사 기능 구현

--- src/main/java/christmas/validator/DateValidator.java
+++ src/main/java/christmas/validator/DateValidator.java
@@ -1,4 +1,34 @@
 package christmas.validator;
 
+import christmas.constant.ErrorMessage;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class DateValidator extends Validator {
+    private static final Pattern VALID_DATE = Pattern.compile("^[0-9]+$");
+
+    @Override
+    public void validate(String inputVisitDate) {
+        validateEmptyInput(inputVisitDate);
+        validateVisitDate(inputVisitDate);
+    }
+
+    private void validateVisitDate(String inputVisitDate) {
+        if(!isValidVisitDate(inputVisitDate)) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_DATE_ERROR.getMessage());
+        }
+    }
+
+    private boolean isValidVisitDate(String inputVisitDate) {
+        Matcher matcher = VALID_DATE.matcher(inputVisitDate);
+        return matcher.matches();
+    }
+
+    public static void validateVisitDateBound(int visitDate) {
+        if(visitDate < 1 || visitDate > 31) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_DATE_ERROR.getMessage());
+        }
+    }
+
 }
